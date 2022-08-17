2f4b8d3b2bbfbee1ae97471551d15a82ab9555d3
Rengod95 <aorwn970@gmail.com>
2022-08-17 18:00:55 +0900

    add request

--- client/crash-talk/src/stores/requester.js
+++ client/crash-talk/src/stores/requester.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 import GV from "./global_variables";
+import { type } from "@testing-library/user-event/dist/type";
 
 const requester = (function () {
   return {
     postUserData: async (header, data) => {
       const response = await axios.post(GV.getServerURL(), {
         headers: header,
+        ContentType: "application/json",
         content: data,
         validity: 0,
       });
+      return response;
+    },
+    joinRoomRequest: async (header, user, room_url) => {
+      const response = await axios.get(room_url, {
+        headers: header,
+        auth: user,
+      });
+
       return response;
     },
   };
