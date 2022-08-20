4dcf8f696fa6c8e808cd7ca2da1132d325474475
KJBig <kjgi73k@gmail.com>
2022-08-20 19:28:35 +0900

    mysql push

--- server/app.js
+++ server/app.js
@@ -3,25 +3,17 @@ const bodyParser = require("body-parser");
 
 const webSocket = require("./socket");
 const indexRouter = require("./routes");
-const { sequelize } = require("./models");
 
 const app = express();
 app.set("port", process.env.PORT || 7727);
 
-// sequelize를 통해 db 연결
-sequelize
-    .sync({ force: false }) // -> true 설정 시 서버 실행할때 마다 테이블 재생성
-    .then(() => {
-        console.log("데이터베이스 연결 성공");
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
 app.use("/", indexRouter);
+
+
 
 // 라우터 존재 x 시
 app.use((req, res, next) => {
@@ -44,3 +36,5 @@ const server = app.listen(app.get("port"), () => {
 });
 
 webSocket(server, app);
+
+//indexRouter(connection);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,25 @@
-const express = require('express');
-//const path = require('path');
+const express = require("express");
+const path = require('path');
+const fs = require('fs');
 const router = express.Router();
+
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database,
+});
+connection.connect();
+
+
+
+//module.exports= (connection) => {
 
 router.get("/", (req, res) => {
     res.send("server is up and running");
@@ -8,39 +27,51 @@ router.get("/", (req, res) => {
 
 router.post("/login", (req, res) => {
     //res.send({ data: { validity: true } });
-    if(req.headers.header === 'LOGIN_USER'){
+    if (req.headers.header === "LOGIN_USER") {
         //DB 확인
         //DB에 겹치는게 있으면
-        res.send({ data : { user: '', validity: true } });
+        res.send({data: {user: "", validity: true}});
     }
-
 });
 
 router.post("/register", (req, res) => {
     //res.send({ data: { validity: true } });
-    if(req.headers.header === 'REGISTER_USER'){
-        //DB 확인
-        //DB에 겹치는게 없으면
-        //req.body.email, req.body.password DB에 push
-        const data ={
-            validity : true,
-        };
-        res.send(data);
+    console.log(req.body);
 
-        console.log(data);
+    if (req.headers.header === "REGISTER_USER") {
+        console.log(req.body);
+        let sql = 'INSERT INTO user VALUES (?, ?, ?, ?, ?)';
 
+        let name = req.body.name;
+        let email = req.body.email;
+        let password = req.body.password;
+        let nick_name = req.body.nickname;
+        let symbol_id = 1
+        let params = [name, email, password, nick_name, symbol_id];
+
+        connection.query(sql, params,
+            (err, rows, fields) => {
+                res.send(false);
+                console.log(err);
+                console.log(rows);
+            })
+
+        // push 성공 시, validity true 반환
+        //
+        // const data = {
+        //   validity: true,
+        // };
+        // res.send(data);
     }
 });
 
 router.post("/home", (req, res) => {
-    if(req.headers.header === ''){
-
+    if (req.headers.header === "JOIN_ROOM") {
     }
 });
 
 router.post("/room", (req, res) => {
-    if(req.headers.header === ''){
-
+    if (req.headers.header === "") {
     }
 });
 
