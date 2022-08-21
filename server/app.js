77b4e45aab8936e7849f5c9e14ba5190d717d082
bigfanoftim <wkdghwns91@gmail.com>
2022-08-22 02:09:50 +0900

    dong

--- server/app.js
+++ server/app.js
@@ -1,37 +1,36 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const webSocket = require("./socket");
-const indexRouter = require("./routes");
+const webSocket = require('./socket');
+const indexRouter = require('./routes');
 
 const app = express();
-app.set("port", process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3001);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 // 라우터 존재 x 시
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
-//error 라우터
+// error 라우터
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
-    res.status(err.status || 500);
-    res.render("error");
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500).end();
 });
 
 //리스너
-const server = app.listen(app.get("port"), () => {
-    console.log(app.get("port"), "번 포트에서 대기 중");
+const server = app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
-webSocket(server, app);
+// webSocket(server, app);
 
 //indexRouter(connection);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require('path');
+const express = require('express');
+// const path = require('path');
 const fs = require('fs');
 const router = express.Router();
 
@@ -8,82 +8,78 @@ const conf = JSON.parse(data);
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: conf.host,
-    user: conf.user,
-    password: conf.password,
-    port: conf.port,
-    database: conf.database,
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
 });
 connection.connect();
 
-router.get("/", (req, res) => {
-    res.send("server is up and running");
+router.get('/', (req, res) => {
+  res.send('server is up and running');
 });
 
-router.post("/login", (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
+router.post('/login', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
 
-    let checkEmail = "SELECT email FROM user WHERE email='" + email + "';";
+  let checkEmail = "SELECT * FROM user WHERE email='" + email + "';";
 
-    connection.query(checkEmail, function (err, rows) {
-        if (rows.length == 1 && req.headers.header === "LOGIN_USER") {
-            let sql = {
-                email: email,
-                password: password,
-            };
-            console.log(sql);
-            res.send('로그인 성공!');
-        }
+  connection.query(checkEmail, function (err, rows) {
+    if (rows.length == 1) {
+      console.log(rows);
+      res
+        .status(200)
+        .json({ message: 'SUCCESS', userNickName: rows[0]['nick_name'] });
+    }
+  });
+});
+
+// req.body
+// 중복 검사
+// email이랑 password validate -> new RegExp
+router.post('/register', (req, res) => {
+  console.log(req.body);
+
+  let name = req.body.name;
+  let email = req.body.email;
+  let password = req.body.password;
+  let nick_name = req.body.nick_name;
+  let symbol_id = null;
+
+  let checkEmail = "SELECT email FROM user WHERE email='" + email + "';";
+
+  connection.query(checkEmail, function (err, rows) {
+    if (rows.length == 0) {
+      let sql = {
+        name: name,
+        email: email,
+        password: password,
+        nick_name: nick_name,
+        symbol_id: symbol_id,
+      };
+
+      connection.query('INSERT INTO user set ?', sql, function (err, rows) {
+        if (err) throw err;
         else {
-            res.send('가입하지 않은 유저입니다');
+          res.send('가입 성공');
         }
-    });
-});
-
-
-router.post("/register", (req, res) => {
-    console.log(req.body);
-
-    let name = req.body.name;
-    let email = req.body.email;
-    let password = req.body.password;
-    let nick_name = req.body.nickname;
-    let symbol_id = null;
-
-    let checkEmail = "SELECT email FROM user WHERE email='" + email + "';";
-
-    connection.query(checkEmail, function (err, rows) {
-        if (rows.length == 0 && req.headers.header === "REGISTER_USER") {
-            let sql = {
-                name: name,
-                email: email,
-                password: password,
-                nick_name: nick_name,
-                symbol_id: symbol_id
-            };
-
-            connection.query('INSERT INTO user set ?', sql, function (err, rows) {
-                if (err) throw err;
-                else {
-                    res.send('가입 성공');
-                }
-            });
-        } else {
-            res.send('중복된 아이디');
-        }
-    });
-});
-
-router.post("/home", (req, res) => {
-    if (req.headers.header === "JOIN_ROOM") {
+      });
+    } else {
+      res.send('중복된 아이디');
     }
+  });
 });
 
-router.post("/room", (req, res) => {
-    if (req.headers.header === "") {
-    }
+router.post('/home', (req, res) => {
+  if (req.headers.header === 'JOIN_ROOM') {
+  }
 });
 
+router.post('/room', (req, res) => {
+  if (req.headers.header === '') {
+  }
+});
 
 module.exports = router;
